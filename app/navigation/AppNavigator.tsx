1d21cbf46f478b0cb561f6ef3199af7d9547f35f
Johnny Rosas <JJRosas@student.fullsail.edu>
2025-05-01 23:14:29 -0400

    made the page title bar transparent to better match the color flow

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,5 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackNavigationOptions, } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
@@ -8,12 +8,21 @@ export type RootStackParamList = {
   Profile: undefined;
 };
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+// Shared header styles
+const commonHeaderOptions: NativeStackNavigationOptions = {
+  headerTransparent: true,
+  headerStyle: { backgroundColor: 'transparent' },
+  headerTitleStyle: { color: '#fff' },
+  headerTitleAlign: 'center',
+};
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home"
+      screenOptions={commonHeaderOptions}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
